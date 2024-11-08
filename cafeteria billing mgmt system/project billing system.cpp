279b03492e723f46864e6b7fa79c37eeb558966e
Bimal <sharmabimal3412@gmail.com>
2024-11-08 14:53:57 +0545

    Project

--- cafeteria billing mgmt system/project billing system.cpp
+++ cafeteria billing mgmt system/project billing system.cpp
@@ -104,3 +104,252 @@ void login()
 		
 }
 
+
+/*start of main*/
+int main()
+{
+	login();
+    d_mainmenu();
+    return 0;
+}
+
+void d_mainmenu()
+{
+    int i;
+    char ch;
+    const char *menu[]= {"   Calculate Bill","   Order Items","   Edit Items","   Display Orders  ","   Search Orders", "   Delete Orders","   Exit"};
+    system("cls");
+//textbackground(11);
+//textcolor(0);
+//_setcursortype(_NOCURSOR);
+    window(25,50,20,32);
+    gotoxy(33,18);
+    printf("CAFE MENU");
+    for (i=0; i<=6; i++)
+    {
+        gotoxy(30,22+i+1);
+        printf("%s\n\n\n",menu[i]);
+    }
+    curser(7);
+}
+
+void d_search()
+{
+    char ch;
+    int i;
+    const char *menu[]= {"   By Code","   By Rate","   By Quantity","   Back to main menu"};
+    system("cls");
+//textbackground(11);
+//textcolor(0);
+    window(25,50,20,32);
+    gotoxy(33,18);
+    printf("SEARCH MENU");
+    for (i=0; i<=3; i++)
+    {
+        gotoxy(30,22+i+1);
+        printf("%s\n\n\n",menu[i]);
+    }
+    curser(4);
+}
+
+/*function for cursor movement*/
+void curser(int no)
+{
+    int count=1;
+    char ch='0';
+    gotoxy(30,23);
+    while(1)
+    {
+        switch(ch)
+        {
+        case 80:
+            count++;
+            if (count==no+1) count=1;
+            break;
+        case 72:
+            count--;
+            if(count==0) count=no;
+            break;
+        }
+        highlight(no,count);
+        ch=getch();
+        if(ch=='\r')
+        {
+            if(no==7)
+            {
+                if (count==1) bill() ;
+                else if(count==2) add();
+                else if(count==3) edit();
+                else if (count==4) d_all();
+                else if (count==5) d_search();
+                else if (count==6) del();
+                else   exit(0);
+            }
+            if(no==4)
+            {
+                if (count==1) d_code();
+                else if (count==2)d_rate();
+                else if (count==3) d_quan();
+                else d_mainmenu();
+            }
+        }
+    }
+}
+
+void highlight(int no,int count)
+{
+    if (no==4)
+    {
+        //textbackground(11);
+        //textcolor(0);
+        gotoxy(30,23);
+        printf("   By Code          ");
+        gotoxy(30,24);
+        printf("   By Rate          ");
+        gotoxy(30,25);
+        printf("   By Quantity      ");
+        gotoxy(30,26);
+        printf("   Back to main menu");
+        //textcolor(0);
+        //textbackground(2);
+        switch (count)
+        {
+        case 1:
+            gotoxy(30,23);
+            printf(" - By Code          ");
+            break;
+        case 2:
+            gotoxy(30,24);
+            printf(" - By Rate          ");
+            break;
+        case 3:
+            gotoxy(30,25);
+            printf(" - By Quantity      ");
+            break;
+        case 4:
+            gotoxy(30,26);
+            printf(" - Back to main menu");
+            break;
+        }
+    }
+
+    if(no==7)
+    {
+        //textbackground(11);
+        //textcolor(0);
+        gotoxy (30,23);
+        printf("   Calculate Bill ");
+        gotoxy (30,24);
+        printf("   Add Orders      ");
+        gotoxy (30,25);
+        printf("   Edit Orders     ");
+        gotoxy (30,26);
+        printf("   Display Orders   ");
+        gotoxy (30,27);
+        printf("   Search         ");
+        gotoxy (30,28);
+        printf("   Delete Orders  ");
+        gotoxy (30,29);
+        printf("   Exit           ");
+        //textcolor(0);
+        //textbackground(2);
+        switch(count)
+        {
+        case 1:
+            gotoxy (30,23);
+            printf(" - Calculate Bill ");
+            break;
+        case 2:
+            gotoxy (30,24);
+            printf(" - Add Orders      ");
+            break;
+        case 3:
+            gotoxy (30,25);
+            printf(" - Edit Orders    ");
+            break;
+        case 4:
+            gotoxy (30,26);
+            printf(" - Display Orders    ");
+            break;
+        case 5:
+            gotoxy (30,27);
+            printf(" - Search         ");
+            break;
+        case 6:
+            gotoxy (30,28);
+            printf(" - Delete Orders   ");
+            break;
+        case 7:
+            gotoxy (30,29);
+            printf(" - Exit           ");
+            break;
+        }
+    }
+}
+
+void bill()
+{
+    char x[4]= {0};
+    int j=29,q=0,size=0,i=1;
+    float total=0,gtotal=0;
+    FILE *file;
+    file=fopen("record.txt","r+b");
+    rewind(file);
+    system("cls");
+    dbill();
+    gotoxy(26,15);
+    printf("Enter  \"end\" to finish input");
+    while(1)
+    {
+        gotoxy(25,18);
+        printf("                    ");
+        gotoxy(25,19);
+        printf("                    ");
+        gotoxy(25,18);
+        printf("Enter Item Code:");
+        scanf("%s",x);
+        if(strcmp(x,"end")==0)
+            break;
+        gotoxy(25,19);
+        printf("Enter Quantity:");
+        scanf("%d",&q);
+        rewind(file);
+        while(fread(&item,sizeof(item),1,file))
+        {
+            if((strcmp(item.code,x)==0))
+            {
+                total=item.rate*q;
+                gotoxy(11,j);
+                printf("%4d",i);
+                printf("%9s",item.name);
+                printf("%13d",q);
+                printf("%15.2f",item.rate);
+                printf("%13.2f",total);
+                gtotal=gtotal+total;
+                size=sizeof(item);
+                item.quantity=item.quantity-q;
+                j+=2;
+                i++;
+                fseek(file,-size,SEEK_CUR);
+                fwrite(&item,sizeof(item),1,file);
+                break;
+            }
+        }
+    }
+    if(gtotal!=0)
+    {
+        gotoxy(30,j+5);
+        printf("TOTAL AMOUNT = NRs. %6.2f",gtotal);
+    }
+    fclose(file);
+    getch();
+    d_mainmenu();
+}
+
+
+
+
+
+
+
+
